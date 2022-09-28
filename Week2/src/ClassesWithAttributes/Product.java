ebe2773d464053565fc101b03824699ac965e165
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-09-28 22:59:05 +0300

    Add Constructor

--- Week2/src/ClassesWithAttributes/Product.java
+++ Week2/src/ClassesWithAttributes/Product.java
@@ -1,6 +1,18 @@
 package ClassesWithAttributes;
 
 public class Product {
+
+    public Product(int id, String name, String description, double price, int stockAmount){
+        System.out.println("Constructor");
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockAmount = stockAmount;
+    }
+
+    public Product(){}
+
     private String name;
     private String description;
     private double price;
