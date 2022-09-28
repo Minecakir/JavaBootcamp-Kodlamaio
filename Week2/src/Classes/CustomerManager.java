2ac0f4aaf5998d56e759502386e69409ba10ea48
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-09-28 21:12:45 +0300

    Complete Classes

---
+++ Week2/src/Classes/CustomerManager.java
@@ -0,0 +1,12 @@
+package Classes;
+
+public class CustomerManager {
+
+    public void Add(){
+        System.out.println("Customer added.");
+    }
+
+    public void Update(){
+        System.out.println("Customer updated");
+    }
+}
