06962b9d144ec1bfd9717d1789e7b6983b6df563
Mine ÇAKIR <cakirrmine00@gmail.com>
2022-09-28 23:41:31 +0300

    Complete MethodOverloading

---
+++ Week2/src/MethodOverloading/Calculator.java
@@ -0,0 +1,6 @@
+package MethodOverloading;
+
+public class Calculator {
+    public int sum(int number1, int number2){return number1 + number2;}
+    public int sum2(int number1,int number2,int number3){return number1+number2+number3;}
+}
---
+++ Week2/src/MethodOverloading/Main.java
@@ -0,0 +1,10 @@
+package MethodOverloading;
+
+public class Main {
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        System.out.println("Summation for two  numbers: "+ calculator.sum(1,2));
+        System.out.println("Summation for three numbers: "+ calculator.sum2(1,2,3));
+
+    }
+}
